72fef3d07a9d26eccadd55d085d044af1eb001de
IlCarretto <carlorebuffat412@gmail.com>
2022-10-27 16:26:20 +0200

    adding tasks dinamically

--- js/script.js
+++ js/script.js
@@ -3,7 +3,31 @@ const {createApp} = Vue;
 createApp({
     data() {
         return {
-            
+            tasks: [
+                {
+                    text: "Fare esercizio",
+                    cancel: "&#x2717;",
+                    done: false
+                },
+                {
+                    text: "Mettere a posto camera",
+                    cancel: "&#x2717;",
+                    done: false
+                },
+                {
+                    text: "Fare la spesa",
+                    cancel: "&#x2717;",
+                    done: false
+                },
+                {
+                    text: "Programmare",
+                    cancel: "&#x2717;",
+                    done: false
+                }
+            ]
         }
+    },
+    methods: {
+
     }
 }).mount("#app");
